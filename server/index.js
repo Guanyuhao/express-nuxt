2a2557382e96b19f0cca3f377f8caa2974c92bba
guanyuhao <272949902@qq.com>
2018-05-11 11:34:22 +0800

    fix

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 const app = express()
-const host = process.env.HOST || '192.168.1.95'
+// const host = process.env.HOST || '192.168.1.95'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
@@ -45,5 +45,7 @@ if (config.dev) {
 app.use(nuxt.render)
 
 // Listen the server
-app.listen(port, host)
-console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+// app.listen(port, host)
+// console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+app.listen(port)
+console.log('Server listening on ' + port) // eslint-disable-line no-console
